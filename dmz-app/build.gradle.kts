3aece6bcdf19f48d319e9175cc646029244c089b
Z-100 <marvin131t@gmail.com>
2024-04-12 10:40:33 +0200

    feature(#1): Change ktor engine

--- dmz-app/build.gradle.kts
+++ dmz-app/build.gradle.kts
@@ -1,6 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val ktor_server_config_yaml_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.23"
@@ -22,9 +23,17 @@ repositories {
 }
 
 dependencies {
+    /* Server dependencies */
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml-jvm:$ktor_server_config_yaml_version")
+
+    /* Postgres */
+
+    /* Solr */
+
+    /* Testing */
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
