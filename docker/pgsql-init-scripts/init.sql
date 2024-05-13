f7671f85bf6f94836a66bcf568e5daa179e2becb
Z-100 <marvin131t@gmail.com>
2024-05-13 16:02:09 +0200

    feature(#1): Add default init.sql data

--- docker/pgsql-init-scripts/init.sql
+++ docker/pgsql-init-scripts/init.sql
@@ -2,6 +2,10 @@ CREATE DATABASE IF NOT EXISTS dmz_pgsql;
 
 \c dmz_pgsql;
 
+
+--
+-- CREATE tables
+--
 create table "user"
 (
     "id"         serial primary key,
@@ -29,3 +33,21 @@ create table "right_user"
     "created_at" timestamp not null default NOW(),
     "updated_at" timestamp not null default NOW()
 );
+
+
+--
+-- INSERT Default info
+--
+
+INSERT INTO "right" (name)
+VALUES ('view-documents'),
+       ('delete-documents');
+
+INSERT INTO "user" (username, password, email, phone)
+VALUES ('admin', 'admin', 'admin@zindustries.ch', '077 777 77 77'),
+       ('user', 'user', 'user@zindustries.ch', '066 666 66 66');
+
+INSERT INTO "right_user" (right_id, user_id)
+VALUES (1, 1),
+       (2, 1),
+       (1, 2);
