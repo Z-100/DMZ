0170b52e9dd04723d039132e5e58f0f62dbae3cc
Z-100 <marvin131t@gmail.com>
2024-05-13 15:04:56 +0200

    feature(#1): Delete document stuff

--- dmz-app/src/main/kotlin/ch/zindustries/dmzapp/DocumentDTO.kt
+++
@@ -1,6 +0,0 @@
-package ch.zindustries.dmzapp
-
-data class DocumentDTO(
-    val name: String,
-    val size: Int,
-)
--- dmz-app/src/main/kotlin/ch/zindustries/dmzapp/DocumentRepository.kt
+++
@@ -1,8 +0,0 @@
-package ch.zindustries.dmzapp
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface DocumentRepository : JpaRepository<Document?, Long?>
-
